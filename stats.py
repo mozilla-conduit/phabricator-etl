67ed46844928b6074f47ffd0ed6c0da709948d8b
Olivier Giorgis <ogiorgis@logilab.fr>
2024-07-04 10:54:03 +0200

    refacto: code industrialization

--- stats.py
+++ stats.py
@@ -6,12 +6,13 @@
 
 import json
 import os
+from dataclasses import dataclass
 from pathlib import Path
 
 import sqlalchemy
 from sqlalchemy import desc, or_
-from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 
 DB_URL = os.environ.get("PHAB_URL", "127.0.0.1")
 DB_NAMESPACE = os.environ.get("PHAB_NAMESPACE", "bitnami_phabricator")
@@ -19,174 +20,247 @@ DB_PORT = os.environ.get("PHAB_PORT", "3307")
 DB_USER = os.environ.get("PHAB_USER", "root")
 DB_TOKEN = os.environ["PHAB_TOKEN"]
 
-Base = automap_base()
 
-# Users
-engine_user = sqlalchemy.create_engine(
-    f"mysql+mysqldb://{DB_USER}:{DB_TOKEN}@{DB_URL}:{DB_PORT}/{DB_NAMESPACE}_user"
-)
-Base.prepare(engine_user)
-session_users = Session(engine_user)
-User = Base.classes.user
+def create_engine(table_suffix):
+    return sqlalchemy.create_engine(
+        f"mysql+mysqldb://{DB_USER}:{DB_TOKEN}@{DB_URL}:{DB_PORT}/{DB_NAMESPACE}_{table_suffix}"
+    )
 
-# Projects
-engine_project = sqlalchemy.create_engine(
-    f"mysql+mysqldb://{DB_USER}:{DB_TOKEN}@{DB_URL}:{DB_PORT}/{DB_NAMESPACE}_project"
-)
-Base.prepare(engine_project)
-session_projects = Session(engine_project)
-Project = Base.classes.project
 
-# Repositories
-engine_repo = engine_user = sqlalchemy.create_engine(
-    f"mysql+mysqldb://{DB_USER}:{DB_TOKEN}@{DB_URL}:{DB_PORT}/{DB_NAMESPACE}_repository"
-)
-Base.prepare(engine_repo)
-session_repo = Session(engine_repo)
-Repo = Base.classes.repository_uri
+def create_engines():
+    return {
+        "user": create_engine("user"),
+        "project": create_engine("project"),
+        "repository": create_engine("repository"),
+        "differential": create_engine("differential"),
+    }
 
-# Diffs
-engine_diffenrential = sqlalchemy.create_engine(
-    f"mysql+mysqldb://{DB_USER}:{DB_TOKEN}@{DB_URL}:{DB_PORT}/{DB_NAMESPACE}_differential"
-)
-Base.prepare(engine_diffenrential)
-session_diff = Session(engine_diffenrential)
-Revision = Base.classes.differential_revision
-Diff = Base.classes.differential_diff
-Changeset = Base.classes.differential_changeset
-Transaction = Base.classes.differential_transaction
-TransactionComment = Base.classes.differential_transaction_comment
-Reviewer = Base.classes.differential_reviewer
-Edges = Base.classes.edge
 
-output = {}
-revisions = session_diff.query(Revision)
-for revision in revisions:
-    rev_key = f"D{revision.id}"
-    output[rev_key] = {}
-    output[rev_key]["first submission timestamp (dateCreated)"] = revision.dateCreated
+def prepare_bases(engines):
+    Base = automap_base()
+    bases = {}
+    for key, engine in engines.items():
+        Base.prepare(engine)
+        bases[key] = Base
+    return bases
+
+
+engines = create_engines()
+bases = prepare_bases(engines)
+
+
+@dataclass
+class UserDb:
+    User = bases["user"].classes.user
+
+
+@dataclass
+class ProjectDb:
+    Project = bases["project"].classes.project
+
+
+@dataclass
+class RepoDb:
+    Repository = bases["repository"].classes.repository_uri
+
+
+@dataclass
+class DiffDb:
+    Revision = bases["differential"].classes.differential_revision
+    Differential = bases["differential"].classes.differential_diff
+    Changeset = bases["differential"].classes.differential_changeset
+    Transaction = bases["differential"].classes.differential_transaction
+    TransactionComment = bases["differential"].classes.differential_transaction_comment
+    Reviewer = bases["differential"].classes.differential_reviewer
+    Edges = bases["differential"].classes.edge
+
+
+def get_last_review_id(revision_phid, session_diff):
     last_review = (
-        session_diff.query(Reviewer)
-        .filter_by(revisionPHID=revision.phid)
+        session_diff.query(DiffDb.Reviewer)
+        .filter_by(revisionPHID=revision_phid)
         .order_by(desc("dateModified"))
         .first()
     )
-    last_review_id = None
-    if last_review:
-        last_review_id = last_review.id
-    output[rev_key]["last review id"] = last_review_id
-    output[rev_key]["current status"] = revision.status
-    repository = session_repo.query(Repo).filter_by(
-        repositoryPHID=revision.repositoryPHID
+    return last_review.id if last_review else None
+
+
+def get_target_repository(repository_phid, session_repo):
+    repository = (
+        session_repo.query(RepoDb.Repository)
+        .filter_by(repositoryPHID=repository_phid)
+        .first()
     )
-    output[rev_key]["target repository"] = repository.first().uri
-    # stack (edge dependencies)
+    return repository.uri
+
+
+def get_stack_size(revision, revisions, session_diff):
     stack = set()
     neighbors = {revision.phid}
     bug_id = revision.title.split("-")[0]
-    while len(neighbors) > 0:
-        query_result = session_diff.query(Edges).filter(
-            or_(Edges.src.in_(neighbors), Edges.src.in_(neighbors)),
-            or_(Edges.type.in_([5, 6])),
+    while neighbors:
+        query_result = (
+            session_diff.query(DiffDb.Edges)
+            .filter(
+                or_(DiffDb.Edges.src.in_(neighbors), DiffDb.Edges.dst.in_(neighbors)),
+                DiffDb.Edges.type.in_([5, 6]),
+            )
+            .all()
         )
         revlist = []
-        for edge in query_result.all():
+        for edge in query_result:
             for rev_src in revisions.filter_by(phid=edge.src):
                 if rev_src.title.split("-")[0] == bug_id:
                     revlist.append(rev_src.phid)
             for rev_dst in revisions.filter_by(phid=edge.dst):
                 if rev_dst.title.split("-")[0] == bug_id:
                     revlist.append(rev_dst.phid)
-        stack = stack | neighbors
+        stack.update(neighbors)
         neighbors = set(revlist) - stack
-    stack_size = len(stack)
-    output[rev_key]["stack size"] = stack_size
-    # diffs
-    output[rev_key]["diffs"] = {}
-    for diff in session_diff.query(Diff).filter_by(revisionID=revision.id):
+    return len(stack)
+
+
+def get_user_name(authorPHID, session_users):
+    user = session_users.query(UserDb.User).filter_by(phid=authorPHID).one()
+    return user.userName
+
+
+def get_review_requests(revision_phid, session_diff, session_projects, session_users):
+    review_requests = {}
+    for review in session_diff.query(DiffDb.Reviewer).filter_by(
+        revisionPHID=revision_phid
+    ):
+        is_reviewer_group = review.reviewerPHID.startswith(b"PHID-PROJ-")
+        if is_reviewer_group:
+            reviewer = (
+                session_projects.query(ProjectDb.Project)
+                .filter_by(phid=review.reviewerPHID)
+                .one()
+            )
+            reviewer_name = reviewer.name
+        else:
+            reviewer = (
+                session_users.query(UserDb.User)
+                .filter_by(phid=review.reviewerPHID)
+                .one()
+            )
+            reviewer_name = reviewer.userName
+        review_requests[f"review-{review.id}"] = {
+            "reviewer": reviewer_name,
+            "is group": is_reviewer_group,
+            "creation timestamp": review.dateCreated,
+            "review timestamp": review.dateModified,
+            "status": review.reviewerStatus,
+        }
+    return review_requests
+
+
+def get_diffs(revision, session_diff, session_users, session_projects):
+    diffs = {}
+    for diff in session_diff.query(DiffDb.Differential).filter_by(
+        revisionID=revision.id
+    ):
         if diff.authorPHID == b"PHID-APPS-PhabricatorDiffusionApplication":
             # ignore diffs that were created as a result of the commit landing.
             continue
         if diff.authorPHID.startswith(b"PHID-RIDT-"):
             # ignore diffs that were created with repository identity
             continue
-        diff_id = f"diff-{diff.id}"
-        current_diff = output[rev_key]["diffs"][diff_id] = {}
-        current_diff["submission time (dateCreated)"] = diff.dateCreated
-        user = session_users.query(User).filter_by(phid=diff.authorPHID).one()
-        current_diff["author (userName)"] = user.userName
-        # changesets
-        current_diff["changesets"] = {}
-        for changeset in session_diff.query(Changeset).filter_by(diffID=diff.id):
-            changeset_id = f"changeset-{changeset.id}"
-            current_diff["changesets"][changeset_id] = {
-                "lines added": changeset.addLines,
-                "lines removed": changeset.delLines,
-            }
-            # comments
-            current_diff["changesets"][changeset_id]["comments"] = {}
-            for comment in session_diff.query(TransactionComment).filter_by(
-                changesetID=changeset.id
-            ):
-                comment_id = f"comment-{comment.id}"
-                user = (
-                    session_users.query(User).filter_by(phid=comment.authorPHID).one()
-                )
-                current_diff["changesets"][changeset_id]["comments"][comment_id] = {
-                    "author": user.userName,
-                    "timestamp (dateCreated)": comment.dateCreated,
-                    "character count": len(comment.content),
-                }
-                att = json.loads(comment.attributes)
-                is_suggestion = False
-                if "inline.state.initial" in att:
-                    hassuggestion = att["inline.state.initial"].get("hassuggestion")
-                    if hassuggestion == "true":
-                        is_suggestion = True
-                        break
-                current_diff["changesets"][changeset_id]["comments"][comment_id][
-                    "is_suggestion"
-                ] = is_suggestion
-        # reviews
-        current_diff["review requests"] = {}
-        for review in session_diff.query(Reviewer).filter_by(
-            revisionPHID=revision.phid
-        ):
-            is_reviewer_group = review.reviewerPHID.startswith(b"PHID-PROJ-")
-            if is_reviewer_group:
-                reviewer = (
-                    session_projects.query(Project)
-                    .filter_by(phid=review.reviewerPHID)
-                    .one()
-                )
-                reviewer_name = reviewer.name
-            else:
-                reviewer = (
-                    session_users.query(User).filter_by(phid=review.reviewerPHID).one()
-                )
-                reviewer_name = reviewer.userName
-            current_diff["review requests"][f"review-{review.id}"] = {
-                "reviewer": reviewer_name,
-                "is group": is_reviewer_group,
-                "creation timestamp": review.dateCreated,
-                "review timestamp": review.dateModified,
-                "status": review.reviewerStatus,
-            }
-    # comments
-    output[rev_key]["comments"] = {}
-    for transaction in session_diff.query(Transaction).filter_by(
-        objectPHID=revision.phid
+        diffs[f"diff-{diff.id}"] = {
+            "submission time (dateCreated)": diff.dateCreated,
+            "author (userName)": get_user_name(diff.authorPHID, session_users),
+            "changesets": get_changesets(diff, session_diff, session_users),
+            "review requests": get_review_requests(
+                revision.phid, session_diff, session_projects, session_users
+            ),
+        }
+    return diffs
+
+
+def get_changesets(diff, session_diff, session_users):
+    changesets = {}
+    for changeset in session_diff.query(DiffDb.Changeset).filter_by(diffID=diff.id):
+        changeset_id = f"changeset-{changeset.id}"
+        changesets[changeset_id] = {
+            "lines added": changeset.addLines,
+            "lines removed": changeset.delLines,
+            "comments": get_changeset_comments(changeset, session_diff, session_users),
+        }
+    return changesets
+
+
+def get_changeset_comments(changeset, session_diff, session_users):
+    comments = {}
+    for comment in session_diff.query(DiffDb.TransactionComment).filter_by(
+        changesetID=changeset.id
+    ):
+        comment_id = f"comment-{comment.id}"
+        user = session_users.query(UserDb.User).filter_by(phid=comment.authorPHID).one()
+        att = json.loads(comment.attributes)
+        is_suggestion = (
+            "inline.state.initial" in att
+            and att["inline.state.initial"].get("hassuggestion") == "true"
+        )
+        comments[comment_id] = {
+            "author": user.userName,
+            "timestamp (dateCreated)": comment.dateCreated,
+            "character count": len(comment.content),
+            "is_suggestion": is_suggestion,
+        }
+    return comments
+
+
+def get_comments(revision_phid, session_diff, session_users):
+    comments = {}
+    for transaction in session_diff.query(DiffDb.Transaction).filter_by(
+        objectPHID=revision_phid
     ):
         if transaction.transactionType == "core:comment":
             comment = (
-                session_diff.query(TransactionComment)
+                session_diff.query(DiffDb.TransactionComment)
                 .filter_by(phid=transaction.commentPHID)
                 .one()
             )
+            user = (
+                session_users.query(UserDb.User)
+                .filter_by(phid=comment.authorPHID)
+                .one()
+            )
             comment_id = f"comment-{comment.id}"
-            output[rev_key]["comments"][comment_id] = {
+            comments[comment_id] = {
                 "author": user.userName,
                 "timestamp (dateCreated)": comment.dateCreated,
                 "character count": len(comment.content),
             }
-Path("revisions.json").write_text(json.dumps(output, indent=2))
+    return comments
+
+
+def process():
+    session_users = Session(engines["user"])
+    session_projects = Session(engines["project"])
+    session_repo = Session(engines["repository"])
+    session_diff = Session(engines["differential"])
+    output = {}
+    revisions = session_diff.query(DiffDb.Revision)
+    for revision in revisions:
+        output[f"D{revision.id}"] = {
+            "first submission timestamp (dateCreated)": revision.dateCreated,
+            "last review id": get_last_review_id(revision.phid, session_diff),
+            "current status": revision.status,
+            "target repository": get_target_repository(
+                revision.repositoryPHID, session_repo
+            ),
+            "stack size": get_stack_size(revision, revisions, session_diff),
+            "diffs": get_diffs(
+                revision,
+                session_diff,
+                session_users,
+                session_projects,
+            ),
+            "comments": get_comments(revision.phid, session_diff, session_users),
+        }
+    Path("revisions.json").write_text(json.dumps(output, indent=2))
+
+
+if __name__ == "__main__":
+    process()
